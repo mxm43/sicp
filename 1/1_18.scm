ddf6dbc9efcf8d56de8f9d0c3cfb65b828954cbe
Mingyi Li <maxime.limy@gmail.com>
2016-04-23 16:51:34 +0800

    add 1_18.scm

---
+++ 1/1_18.scm
@@ -0,0 +1,12 @@
+(define (double n) (+ n n))
+(define (halve n) (/ n 2))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (mul a b)
+  (define (iterator r a b)
+    (cond ((= b 0) r)
+          ((even? b) (iterator r (double a) (halve b)))
+          (else (iterator (+ r a) a (- b 1)))))
+  (iterator 0 a b))
