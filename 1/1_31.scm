b1c22dc1c0ef3b884c1d385e7f0271791333d94d
Mingyi Li <maxime.limy@gmail.com>
2016-04-24 21:32:23 +0800

    add 1_32.scm

--- 1/1_31.scm
+++ 1/1_31.scm
@@ -3,12 +3,19 @@
       1
       (* (term a) (product-recu term (next a) next b))))
 
+(define (product-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result 
+        (iter (next a) (* result (term a)))))
+  (iter a 1))
+
 (define (square x) (* x x))
 
-(define (factorial-recu n)
+(define (factorial n)
   (define (term k)
     (/ (square k) (square (- k 1))))
   (define (next k)
     (+ k 2))
   
-  (/ (* 8.0 (product-recu term 4 next n)) n))
+  (/ (* 8.0 (product-iter term 4 next n)) n))
