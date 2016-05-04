0ecd17d497248f1c669b5f8742cb678af08ee144
Mingyi Li <maxime.limy@gmail.com>
2016-05-04 14:15:07 +0800

    add 2_2.scm

---
+++ 2/2_2.scm
@@ -0,0 +1,34 @@
+(define (average v1 v2)
+  (/ (+ v1 v2) 2))
+    
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (midpoint-segment segment)
+  (make-point (average (x-point (start-segment segment))
+                       (x-point (end-segment segment)))
+              (average (y-point (start-segment segment))
+                       (y-point (end-segment segment)))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
