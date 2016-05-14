5c66385e7e7ee0b00b7468f52e01aa2f3f730552
Mingyi Li <maxime.limy@gmail.com>
2016-05-14 21:47:35 +0800

    add 2_13.scm

---
+++ 2/2_13.scm
@@ -0,0 +1,6 @@
+; percent = [(a + a * p) * (b + b * q) - (a - a * p) * (b - b * q)] /
+;           [(a + a * p) * (b + b * q) + (a - a * p) * (b - b * q)]
+;         = (p + q) / (1 + p * q)
+;
+; if p and q are small enough, p * q approximately equals to 0, percent
+; approximately equals to (p + q).
