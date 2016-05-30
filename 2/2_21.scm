22fd14efaf2cf8849f28edd0686f90587426296d
Mingyi Li <maxime.limy@gmail.com>
2016-05-30 23:52:43 +0800

    add 2_21.scm

---
+++ 2/2_21.scm
@@ -0,0 +1,9 @@
+(define (square n) (* n n))
+    
+(define (square-list items)
+  (if (null? items)
+      '()
+      (cons (square (car items)) (square-list (cdr items)))))
+
+(define (square-list items)
+  (map square items))
