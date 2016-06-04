4ddc9c955e7417f579951a895aeee3efbfd41d8f
Mingyi Li <maxime.limy@gmail.com>
2016-06-04 23:16:55 +0800

    add 2_30.scm

---
+++ 2/2_30.scm
@@ -0,0 +1,8 @@
+(define (square n) (* n n))
+    
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree sub-tree)
+             (square sub-tree)))
+       tree))
